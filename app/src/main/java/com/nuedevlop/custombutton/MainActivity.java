284c4b33f3145d5990b36a61a32db10554f8d0a8
m haqqul yakin <imanueyatsuke@gmail.com>
2019-11-13 19:49:08 +0700

    Initial commit

---
+++ app/src/main/java/com/nuedevlop/custombutton/MainActivity.java
@@ -0,0 +1,58 @@
+package com.nuedevlop.custombutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnCoba;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnCoba = findViewById(R.id.btnCoba);
+
+        btnCoba.setOnClickListener(v->Ganti());
+
+
+    }
+
+    private void Ganti() {
+        final Animation myAnim = AnimationUtils.loadAnimation(this, R.anim.bounce);
+
+        // Use bounce interpolator with amplitude 0.2 and frequency 20
+        MyBounceInterpolator interpolator = new MyBounceInterpolator(0.2, 20);
+        myAnim.setInterpolator(interpolator);
+        myAnim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+               toast();
+
+            }
+
+            private void toast() {
+                Toast.makeText(MainActivity.this,
+                        "tombol di tekan",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        btnCoba.startAnimation(myAnim);
+
+
+
+    }
+}
